a17df3b2e8d2e29a9f0f9fecf1c5e0ae3b1ab4af
crisnav21 <kavinesh.kk@gmail.com>
2025-11-19 15:14:06 +0800

    fixed permission

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 from rest_framework import status
 
@@ -7,6 +8,7 @@ from .serializers import ProductSerializer
 
 
 @api_view(["GET"])
+@permission_classes([AllowAny])  # 👈 public
 def product_list(request):
     products = Product.objects.all().order_by("id")
     serializer = ProductSerializer(products, many=True)
@@ -14,12 +16,14 @@ def product_list(request):
 
 
 @api_view(["GET"])
+@permission_classes([AllowAny])  # 👈 public
 def product_detail(request, pk):
     try:
         product = Product.objects.get(pk=pk)
     except Product.DoesNotExist:
         return Response(
-            {"message": "Product not found"}, status=status.HTTP_404_NOT_FOUND
+            {"message": "Product not found"},
+            status=status.HTTP_404_NOT_FOUND,
         )
 
     serializer = ProductSerializer(product)
